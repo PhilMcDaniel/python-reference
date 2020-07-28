22f1704dc1817cba9596ba9e20a72a64db7cdffa
Phil McDaniel <mcdanipc@gmail.com>
2020-07-28 09:52:35 -0500

    PCM add .bat and make mouse click

---
+++ mousemover.bat
@@ -0,0 +1,4 @@
+:loop
+start C:/Users/phil_/AppData/Local/Programs/Python/Python38-32/python.exe c:/Users/phil_/OneDrive/Documents/GitHub/python-reference/mousemover.py
+timeout /t 30 /nobreak
+goto :loop
