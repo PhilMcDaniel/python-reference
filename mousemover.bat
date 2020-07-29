2852a8c88cdbbeb2c8e2505062ee67af26b6c99f
Phil McDaniel <mcdanipc@gmail.com>
2020-07-29 08:36:46 -0500

    Update mousemover.bat
    Change loop length to 240 sec (4min)

--- mousemover.bat
+++ mousemover.bat
@@ -1,4 +1,4 @@
 :loop
 start C:/Users/phil_/AppData/Local/Programs/Python/Python38-32/python.exe c:/Users/phil_/OneDrive/Documents/GitHub/python-reference/mousemover.py
-timeout /t 30 /nobreak
+timeout /t 240 /nobreak
 goto :loop
