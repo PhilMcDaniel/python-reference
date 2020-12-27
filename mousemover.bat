af487216ca2b210536d6ec86f0a1d7e44374f63d
Phil McDaniel <mcdanipc@gmail.com>
2020-12-27 10:08:27 -0600

    Update mousemover.bat

--- mousemover.bat
+++ mousemover.bat
@@ -1,4 +1,4 @@
 :loop
-start C:/Users/phil_/AppData/Local/Programs/Python/Python39/python.exe c:/Users/phil_/OneDrive/Documents/GitHub/python-reference/mousemover.py
+start C:/Users/phil_/AppData/Local/Programs/Python/Python39/pythonw.exe c:/Users/phil_/OneDrive/Documents/GitHub/python-reference/mousemover.py
 timeout /t 90 /nobreak
 goto :loop
